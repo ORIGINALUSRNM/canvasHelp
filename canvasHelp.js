8224f331a788f97c16bc8a76caf48c793a1d7ba6
Jason Hornbuckle <jshornbuckle@gmail.com>
2013-02-13 18:48:04 -0500

    first commit

---
+++ canvasHelp.js
@@ -0,0 +1,42 @@
+(function(){
+
+	var canvasHelp;
+
+	var getContext = function(canvasId) {
+		var canvas = document.getElementById(canvasId);
+		return canvas.getContext('2d');
+	};
+	var drawLine = function( ctx, line ) { 
+		ctx.beginPath();
+		ctx.moveTo(line.start.x, line.start.y);
+		ctx.lineTo(line.end.x, line.end.y);
+		ctx.stroke();
+		ctx.closePath();
+	};
+
+	var Point = function( x, y ) {
+		if(!(this instanceof Point)){
+			return new canvasHelp.Point( x, y ); 
+		}
+		this.x = x;
+		this.y = y;
+	};
+
+	var Line = function( start, end ) {
+		if(!(this instanceof Line)){
+			return new canvasHelp.Line( start, end );
+		}
+		this.start = start;
+		this.end = end;
+	}
+
+	canvasHelp = {
+		getContext : getContext,
+		drawLine : drawLine, 
+		Point : Point, 
+		Line : Line
+	};
+
+	window.canvasHelp = $ch = canvasHelp;
+
+})();
